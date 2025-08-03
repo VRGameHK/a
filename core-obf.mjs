ac47be9f1719e04cbc6757c7a9f9a3b91e6ebf75
VRGameHK <junyan459427@gmail.com>
2025-08-03 21:35:53 +0800

    Update core-obf.mjs

--- core-obf.mjs
+++ core-obf.mjs
@@ -1,9 +1,10 @@
 export const z = (() => {
   const x = (str, key) =>
     str.split('').map((c, i) => String.fromCharCode(c.charCodeAt(0) ^ key.charCodeAt(i % key.length))).join('');
-  const xorPayload = '\u0012\u0019\u0011\u0016T\u0005\u000e\u000eU^\u000b\u001f\u0006T\u0013\u0010E^\u0012\u0011\u0005\t\u0006\u0011';
+  const xorPayload = '\u0012\u0019\u0011\u0016T\u0005\u000e\u000eU^\u000b\u001f\u0006T\u0013\u0010C^\u0012\u0011\u0005\t\u0006\u0011';
   const key = 'dream';
   const payload = x(xorPayload, key);
+
   const antiDebug = () => {
     let detected = false;
     const test = () => {
@@ -19,6 +20,7 @@ export const z = (() => {
   const freezePage = () => {
     while (true) console.log(Math.random());
   };
+
   const protect = () => {
     if (antiDebug()) {
       freezePage();
